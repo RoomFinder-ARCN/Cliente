6c51e7eabc4367b23299ae16dacda164c567d973
LuisaGiron <luisa.bermudez@mail.escuelaing.edu.co>
2024-04-04 17:53:26 -0500

    proyecto base del servicio cliente

---
+++ src/test/java/arcn/roomfinder/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package arcn.roomfinder.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
